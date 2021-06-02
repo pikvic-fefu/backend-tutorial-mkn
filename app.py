a956db10146cfdc074fc195a681f30e209e63dcc
pikvic <truepikvic@gmail.com>
2021-06-03 09:44:33 +1000

    Add posts feature

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
+posts = [
+    {"title": "djksjd", "body": "asdjasdlljdajdsl"},
+    {"title": "djk21332132sjd", "body": "asdj123231asdlljdajdsl"},
+]
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    context = {"time": datetime.datetime.now(), "posts": posts}
+    return render_template('index.html', **context)
