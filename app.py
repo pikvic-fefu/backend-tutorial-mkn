d792d85a82e5481f697f9e8bd7caf5e60a4cd025
pikvic <truepikvic@gmail.com>
2021-06-04 11:14:05 +1000

    Add users and comments

--- app.py
+++ app.py
@@ -5,10 +5,24 @@ import json
 
 app = Flask(__name__)
 
-posts = json.loads(requests.get("https://jsonplaceholder.typicode.com/posts").text)
+def get_posts():
+    posts = json.loads(requests.get("https://jsonplaceholder.typicode.com/posts").text)
+    users = json.loads(requests.get("https://jsonplaceholder.typicode.com/users").text)
+    comments = json.loads(requests.get("https://jsonplaceholder.typicode.com/comments").text)
 
+    for post in posts:
+        for user in users:
+            if post["userId"] == user["id"]:
+                post["user"] = user["name"]
+        post["comments"] = []
+        for comment in comments:
+            if post["id"] == comment["postId"]:
+                post["comments"].append(comment)
+        post["comments_count"] = len(post["comments"])
+    return posts
 
 @app.route("/")
 def index():
+    posts = get_posts()
     context = {"time": datetime.datetime.now(), "posts": posts}
     return render_template('index.html', **context)
